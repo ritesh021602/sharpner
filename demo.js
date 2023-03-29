9478bfe6dd8278584c9030d90b062d54a09fc255
ritesh021602 <75390604+ritesh021602@users.noreply.github.com>
2023-03-29 12:48:01 +0530

    third commit

--- demo.js
+++ demo.js
@@ -1,3 +1,9 @@
 let h=document.getElementById('main-header');
 h.style.borderBottom='solid 3px #000';
 console.log("hi");
+let items=document.getElementsByClassName('list-group-item');
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+    items[i].style.backgroundColor="#f4f4f4";
+}
+items[2].style.color="green";
